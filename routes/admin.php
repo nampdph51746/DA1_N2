5e701032e7d9cf5059ab105eb79c67c56792f763
Nguyen-Hieu1409 <hieunnph51707@gmail.com>
2025-03-22 17:27:42 +0700

    Add files via upload

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,25 @@
 <?php
-
-$router->get('/admin', function () {
-    return "View Admin";
+use App\Controllers\Admin\CategoryController;
+use App\Controllers\Admin\ProductController;
+// $router->get('/admin', function () {
+//     return "View Admin";
+// });
+$router->group(['before'=>'admin'], function($router){
+    $router->group(['prefix' => 'admin'], function ($router) {
+         //danh mục
+        $router->get('/categories', [CategoryController::class,'index']);
+        $router->get('/categories/create', [CategoryController::class,'create']);
+        $router->post('/categories/store', [CategoryController::class,'store']);
+        $router->get('/categories/edit/{id}', [CategoryController::class,'edit']);
+        $router->post('/categories/update{id}', [CategoryController::class,'update']);
+        $router->get('/categories/delete/{id}', [CategoryController::class,'destroy']);
+       
+        // sản phẩm
+        $router->get('products',[ProductController::class,'index']);
+        $router->get('products/create',[ProductController::class,'create']);
+        $router->post('products/store',[ProductController::class,'store']);
+        $router->get('products/edit/{id}',[ProductController::class,'edit']);
+        $router->post('products/update/{id}',[ProductController::class,'update']);
+        $router->get('products/delete/{id}',[ProductController::class,'destroy']);
+    });
 });
