23f4efb6ce337ec51fa22f2522b708263f7797e0
Nguyen-Hieu1409 <hieunnph51707@gmail.com>
2025-03-27 17:35:22 +0700

    thêm mới

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -4,11 +4,11 @@ use App\Models\User;
 class UserController {
     public function index(){
         $users = User::all();
-        return view("Admin.User.index",compact("users"));
+        return view("Admin.user.list",compact("users"));
     }
     public function search() {
         $query =trim($_GET['query'] ?? '');
         $users= User::where('username','like','%'.$query.'%')->get();
-        return view('Admin.User.index',compact('users'));
+        return view('Admin.user.list',compact('users'));
     }
 }
